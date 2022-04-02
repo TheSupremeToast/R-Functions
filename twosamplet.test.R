13b2b3d948fce385494815724671ecba5d21ab62
TheSupremeToast <patrickdewey2002@gmail.com>
2022-04-01 22:26:47 -0400

    Initial commit

---
+++ twosamplet.test.R
@@ -0,0 +1,134 @@
+# Patrick Dewey - CMDA2006 - Homework 2
+twosamplet.test <- function(x, y, conf.level = 0.95, tail = "both", test.type = "welch"){
+    # input flag options
+    # conf.level
+    # test.type: welch, pooled, paired
+    # tails: left, right, both
+
+    # assign dummy values to avoid scope issues
+    ts = 0
+    df = 0
+    p = 0
+    s_err = 0
+    lb = 0
+    ub = 0
+    t_score = 0
+
+    if (test.type == 'welch')
+    {
+        # welch t.test
+        sxnx = sd(x)^2 / length(x) 
+        syny = sd(y)^2 / length(y) 
+        
+        # calculate the standard error
+        s_err = sqrt(sxnx + syny)
+
+        # calculate the degrees of freedom
+        df_num = (sxnx + syny) ^ 2
+        df_denom = (((sxnx)^2 / (length(x) - 1)) + ((syny)^2 / (length(x) - 1)))
+        df = df_num / df_denom 
+
+        mean_diff = mean(x) - mean(y)
+
+        # calculate the t statistic
+        ts = mean_diff / s_err
+        
+
+    } else if (test.type == 'pooled')
+    {
+        # pooled t.test
+        sx = sd(x)
+        sy = sd(y)
+        nx = length(x)
+        ny = length(y)
+
+        # calculate the degrees of freedom 
+        df = nx + ny - 2
+
+        # calculate standard error
+        sp_num = ((nx - 1) * sx ^ 2) + ((ny - 1) * sy ^ 2)
+        sp = sqrt(sp_num / df)
+        s_err = sp * sqrt((1 / nx) + (1 / ny))
+
+        mean_diff = mean(x) - mean(y)
+        
+        # calcualte the test statistic
+        ts = mean_diff / s_err 
+
+    } else if (test.type == 'paired')
+    {
+        # paired t.test
+        D = x - y
+
+        # standard error
+        s_err = sd(D) / (sqrt(length(D)))
+
+        # degrees of freedom
+        df = length(D) - 1
+
+        # test statistic
+        ts = mean(D) / s_err
+
+        mean_diff = mean(D)
+    }
+
+    # define alpha
+    alpha = 1 - conf.level
+
+    # p-value & confidence level
+    if (tail == 'left')
+    {
+        # p-value
+        p = pt(ts, df) 
+        # t-score for confidence interval
+        t_score = qt(1 - alpha, df)
+
+        # confidence interval upper and lower bounds
+        ub = mean_diff - (t_score * s_err)
+        lb = '-Infinity' 
+
+    } else if (tail == 'right')
+    {
+        # p-value
+        p = pt(ts, df, lower.tail = F)
+        # t-score
+        t_score = qt(1 - alpha, df, lower.tail = F)
+
+        # confidence interval upper and lower bounds
+        lb = mean_diff + (t_score * s_err)
+        ub = 'Infinity'
+
+    } else if (tail == 'both')
+    {
+        # p-value
+        p = 2 * pt(abs(ts), df, lower.tail = F)
+        # t-score
+        t_score = qt(1 - alpha/2, df, lower.tail = F) # TODO - outputs t_score not ci
+
+        # confidence interval upper and lower bounds
+        ub = mean_diff - (t_score * s_err)
+        lb = mean_diff + (t_score * s_err)
+    }
+
+
+
+    #returns a table containing:
+    # test statistic
+    # degrees of freedom
+    # the p-value
+    # confidence level
+    # the lower bound of the confidence interval
+    # upper bound of the confidence interval
+
+    # output but not in table:
+    # type of test -> see test.type
+    # direction of the test
+
+    # create output table
+    result <- cbind('Test Statistic' = ts, 'Degrees of Freedom' = df, 'P-value'= p, 
+        'Confidence Level' = conf.level, 'Lower Bound' = lb, ' Upper Bound' = ub)
+
+    return(list("results" = result, 
+                "testtype:" = test.type, 
+                "directon:" = tail)) 
+}
---
+++ wilcox.R
@@ -0,0 +1,300 @@
+# Patrick Dewey - CMDA2006 - Homework 3
+#
+# mywilcox.test
+
+# TODO:
+# tie cases for single sample
+# hypothesis test - Use mu to check?
+mywilcox.test <- function(x, y = NULL, alpha = .05, mu = 0, tail = 'both', paired = FALSE)
+{
+
+    # Shapiro-wilks test for normality
+    normal <- FALSE
+    # shapiro-wilks test for x
+    sw_px <- (shapiro.test(x))[2]
+    # shapiro-wilks test for y
+    sw_py <- 'NA' 
+    if (!is.null(y))
+    {
+        sw_py <- (shapiro.test(y))[2]
+    }
+
+    # check normality and move forwards accordingly
+    if (sw_px >= 0.10 && sw_py >= 0.10 | sw_px >= 0.10 && is.null(y))
+    {
+        normal <- TRUE
+        # print('Data is normal, use a t-test instead.')
+    }
+
+    # define dummy variables
+    z_s <- NULL 
+    W <- NULL
+    p <- 1
+    hyp <- NULL 
+
+    # must be able to do:
+    # signed rank test for one sample data (test h0: u = u0 vs ha: u != u0)
+    # signed rank test for paired samples
+    # mann-whitney U test for 2 independent samples
+
+    # figure out test type - this is separate from the lower if statements for aesthetic reasons
+    # if y != null then there are multiple samples
+    mann_whitney <- FALSE 
+    single_mean_wilcoxon <- FALSE
+    if (is.null(y))
+    {
+        single_mean_wilcoxon <- TRUE 
+
+    } else if (paired == FALSE) # not paired + not single = mann_whitney
+    {
+        mann_whitney <- TRUE 
+    }
+
+
+#######################################
+### Mann Whitney signed-rank U Test ###
+#######################################
+
+    if (mann_whitney)
+    {
+        x <- sort(x)
+        y <- sort(y)
+        k_x <- 0;
+        k_y <- 0;
+        nx <- length(x)
+        ny <- length(y)
+
+        # rank-sum algorithm
+        for (entry in x)
+        {
+            k_x <- k_x + length(which(y < entry))
+            k_x <- k_x + 0.5 * length(which(y == entry))
+            
+            k_y <- k_y + length(which(y > entry))
+            k_y <- k_y + 0.5 * length(which(y == entry))
+        }
+
+        # Find test statistic U_s
+        U_s = min(k_x, k_y)
+    
+        # check direction and test appropriately
+        if (tail == 'both')
+        {
+            numerator <- abs(U_s - ((nx * ny) / 2)) - 0.5 
+            denominator <- sqrt((nx * ny * (nx + ny + 1)) / 12)
+
+            z_s <- numerator / denominator
+
+            p <- 2 * pnorm(z_s, lower.tail = FALSE)
+
+        } else if (tail == 'left')
+        {
+            numerator <- abs(U_s - ((nx * ny) / 2)) - 0.5 
+            denominator <- sqrt((nx * ny * (nx + ny + 1)) / 12)
+
+            z_s <- numerator / denominator
+
+            p <- pnorm(z_s, lower.tail = FALSE)
+
+        } else if (tail == 'right')
+        {
+            numerator <- U_s - ((nx * ny) / 2) - 0.5 
+            denominator <- sqrt((nx * ny * (nx + ny + 1)) / 12)
+
+            z_s <- numerator / denominator
+
+            p <- pnorm(z_s, lower.tail = FALSE)
+        }
+        W <- U_s # convert test statistic to W for final return statement
+
+        # Hypothesis test
+        if (p > alpha)
+        {
+            hyp <- 'We fail to reject H0.'
+
+        } else if (p <= alpha)
+        {
+            hyp <- 'We reject H0'
+        }
+    }
+
+####################################################
+### Wilcoxon signed-rank test for paired samples ###
+####################################################
+
+    if (paired)
+    {
+        diff <- x - y
+        # remove data points where 'x - y = 0'
+        diff <- subset(diff, diff != 0)
+        nx <- length(diff)
+        ny <- length(diff)
+        abs_diff <- abs(diff)
+        abs_rank <- NULL
+
+        # I didn't realize there was a rank function built into R until I got a
+        # lot farther into the process 
+        # calculate the rank of |d|
+        abs_rank <- rank(abs_diff, ties.method = 'average')
+
+        # Deal with storing where +/- are
+        signs <- NULL
+        for (entry in diff)
+        {
+            if (entry > 0)
+            {
+                signs <- c(signs, 1)  
+            } else if (entry < 0)
+            {
+                signs <- c(signs, -1)         
+            }
+        }
+        
+        # reapply +/- to 'sign' ranks
+        signed_rank <- abs_rank * signs
+
+        # sum positive ranks, sum abs of negative ranks
+        Wplus  <- sum(signed_rank[which(signed_rank > 0)])
+        Wminus <- sum(signed_rank[which(signed_rank < 0)])
+        # test statistic = min(W_+, W_-)
+        W_s <- min(Wplus, abs(Wminus))
+        n <- nx
+        
+        # test statistic z_s and p-value
+        if (tail == 'both'){
+            numerator <- abs(W_s - (n * (n + 1) / 4)) - 0.5
+            denominator <- sqrt((n * (n + 1/2) * (n + 1)) / 12)
+            z_s <- numerator / denominator
+            p <- 2 * pnorm(z_s, lower.tail = FALSE)
+
+        } else if(tail == 'left'){
+            numerator <- W_s - (n * (n + 1) / 4) - 0.5
+            denominator <- sqrt((n * (n + 1/2) * (n + 1)) / 12)
+            z_s <- numerator / denominator
+            p <- pnorm(z_s, lower.tail = FALSE)
+
+        } else if (tail == 'right'){
+            numerator <- abs(W_s - (n * (n + 1) / 4)) - 0.5
+            denominator <- sqrt((n * (n + 1/2) * (n + 1)) / 12)
+            z_s <- numerator / denominator
+            p <- pnorm(z_s, lower.tail = FALSE)
+        }
+        
+        W <- W_s # end of function return requires W
+
+        # Hypothesis test
+        if (p > alpha)
+        {
+            hyp <- 'We fail to reject H0.'
+
+        } else if (p <= alpha)
+        {
+            hyp <- 'We reject H0'
+        }
+    }
+
+ 
+#################################
+### Single mean wilcoxon test ###
+#################################
+
+    if (single_mean_wilcoxon)
+    {
+        if (mu == 0) {
+            mu = 12
+        }
+
+        n <- length(x)
+        diff <- x - mu 
+        # diff <- subset(diff, diff != 0)
+        abs_diff <- abs(diff)
+        Wplus <- 0
+        Wminus <- 0
+
+        # get the unsigned rank
+        abs_rank <- rank(abs_diff, ties.method = 'average')
+
+        # get the position of signs
+        signs <- NULL
+        for (entry in diff)
+        {
+            if (entry >= 0)
+            {
+                signs <- c(signs, 1) 
+
+            } else if (entry < 0)
+            {
+                signs <- c(signs, -1) 
+            }
+        }
+
+        # sign rank
+        signed_rank <- abs_rank * signs
+
+        # find W+/W-
+        for (entry in signed_rank)
+        {
+            if (entry >= 0)
+            {
+                Wplus <- Wplus + entry
+
+            } else if (entry <= 0) 
+            {
+                Wminus <- Wminus + entry
+            }
+        }
+
+
+        # Test statistic
+        W_s <- min(Wplus, abs(Wminus))
+        print(W_s)
+
+        # Standardized test statistic same formula for each tail type
+        numerator <- abs(W_s - (n * (n + 1)/4)) - 0.5
+        denominator <- sqrt((n * (n + 1/2) * (n + 1)) / 12)
+        z_s <- numerator / denominator
+
+        if (tail == 'both'){
+            p <- 2 * pnorm(z_s, lower.tail = FALSE)
+
+        } else if (tail == 'left'){
+            p <- 1 - pnorm(z_s)
+
+        } else if (tail == 'right'){
+            p <- pnorm(z_s, lower.tail = FALSE)
+        }
+
+        print(z_s)
+
+        W <- W_s
+
+        # Hypothesis test
+        if (p > alpha)
+        {
+            hyp <- 'We fail to reject H0.'
+
+        } else if (p <= alpha)
+        {
+            hyp <- 'We reject H0'
+        }
+    }
+
+#################################
+
+    # Return a list containing results from hypothesis test
+    # - contains table with test statistic based on ranks, W
+    # - standardized test staistic, z_s
+    # - p-value based on the approximation method and using z_s
+    results <- cbind('Test Statistic' = W, 'Standardized Test Statistic' = z_s, 'P-value' = p)    
+
+    # other things to return in list
+    # directon of the test, type of test used,
+    # whether or not H0 is rejected
+    # p-value from shapiro-wilk's test
+    return(list('results' = results, 
+                'test_direction' = tail,
+                'hypothesis test result' = hyp,
+                'Shapiro_Wilks_p_x' = sw_px,
+                'Shapiro_Wilks_p_y' = sw_py,
+                'Shapiro_Wilks_normality' = normal))
+}
